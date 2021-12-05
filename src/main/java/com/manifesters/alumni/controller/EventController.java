ab35ba23c19f960a6a7f8c1f9f997ea6fa66470b
ridhimak29 <90280365+ridhimak29@users.noreply.github.com>
2021-12-04 19:53:09 -0500

    Added buy ticket functionality and pass values to transaction.html

--- src/main/java/com/manifesters/alumni/controller/EventController.java
+++ src/main/java/com/manifesters/alumni/controller/EventController.java
@@ -23,7 +23,7 @@ public class EventController {
     @Autowired
     private EventService service;
 
-    @GetMapping("/event")
+    @GetMapping("/events")
     public String populateEvents(Model model){
         List<Event> events  = service.getAllEvents(0, 15, "date");
         model.addAttribute("events", events );
@@ -38,7 +38,21 @@ public class EventController {
         // Create a new event with imagePath
         Event newEvent = new Event(event.getEventName(), event.getVenue(), event.getDescription(), event.getDate(), fileName, event.getPrice());
         service.saveEvent(newEvent);
-        return "redirect:/event";
+        return "redirect:/events";
+    }
+
+    @PostMapping(value = "/buy_ticket")
+    public String buyTicket(@CookieValue(value = "JSESSIONID", defaultValue = "55b83b76-2741-4b0c-9df1-c07ee4dec646") String sessionId,
+                            @RequestParam String id, @RequestParam String price, @RequestParam String quantity, Model model){
+        System.out.println( "Session Id:"+ sessionId);
+        model.addAttribute("eventId", id );
+        model.addAttribute("eventPrice", price);
+        model.addAttribute("purchaseQuantity", quantity);
+        if (!StringUtils.hasText(sessionId)){
+            model.addAttribute("forwardTo", "transaction");
+            return "login";
+        }
+        return "transaction";
     }
 
     @InitBinder
