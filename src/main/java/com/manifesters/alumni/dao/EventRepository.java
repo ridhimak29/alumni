16116d06a87219d055e0bd74eb39741eb31a2cd7
ridhimak29 <90280365+ridhimak29@users.noreply.github.com>
2021-12-04 19:53:09 -0500

    Added buy ticket functionality and pass values to transaction.html

--- src/main/java/com/manifesters/alumni/dao/EventRepository.java
+++ src/main/java/com/manifesters/alumni/dao/EventRepository.java
@@ -1,11 +1,12 @@
 package com.manifesters.alumni.dao;
 
 import com.manifesters.alumni.types.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EventRepository extends PagingAndSortingRepository<Event,Long>, CrudRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, Long> {
 
 }
